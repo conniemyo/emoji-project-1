af5af0652732b5a0d7fda3916627ff3cf37d2156
conniemyo <connieyoungxo@gmail.com>
2025-04-05 09:57:28 -0500

    last changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
 
-// Add event listener
 closedFace.addEventListener('click', () => {
   if (openFace.classList.contains('open')) {
     openFace.classList.add('active');
